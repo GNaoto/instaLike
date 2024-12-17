ea9cfef457ec32a1c0a4b1d95a2e759b839e2c4e
gnaot <gnaoto1000@gmail.com>
2024-12-16 23:16:57 -0300

    primeiro commit

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import routes from "./src/routes/postsRoutes.js";
+
+const app = express();
+app.use(express.static("uploads"))
+routes(app)
+
+// Inicia o servidor na porta 3000 e exibe uma mensagem no console
+app.listen(3000, () => {
+    console.log("Servidor escutando...");
+});
+
+
+// function buscarPostPorID(id){
+//     return posts.findIndex((post) =>{
+//         return post.id === Number(id)
+//     })
+// }
+
+// app.get("/posts/:id", (req, res)=>{
+//     const index = buscarPostPorID(req.params.id)
+//     res.status(200).json(posts[index]);
+// });
